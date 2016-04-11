2aa7ffa6b1e63d213a64fdbc34d4fab8b1cb970e
Royi Reshef <rreshef@cisco.com>
2016-04-11 23:32:55 +0300

    add julian

--- hdate.py
+++ hdate.py
@@ -1,3 +1,4 @@
+import hdate_julian as hj
 import datetime
 
 
@@ -11,122 +12,234 @@ PARTS_IN_MONTH = PARTS_IN_DAY + M(12, 793)  # Tikun for regular month
 
 
 class HDate(object):
-
     def __init__(self, date=None):
         if date is None:
             date = datetime.date.today()
         elif not isinstance(date, datetime.date):
             raise TypeError
-        self._date = date
-
-    def _M(self, h, p):
-        return (h * self.PARTS_IN_HOUR) + p
-        
-    def _days_since_3744(self, hebrew_year):
-        """Return: Number of days since 3,1,3744 """
-        # Start point for calculation is Molad new year 3744 (16BC)
-        years_from_3744 = hebrew_year - 3744
-        molad_3744 = M(1 + 6, 779)  # Molad 3744 + 6 hours in parts
-        # Time in months
-        leap_months = (years_from_3744 * 7 + 1) / 19  # Number of leap months
-        leap_left = (years_from_3744 * 7 + 1) % 19  # Months left of leap cycle
-        months = years_from_3744 * 12 + leap_months  # Total Number of months
-        # Time in parts and days
-        parts = months * PARTS_IN_MONTH + molad_3744  # Molad This year + Molad 3744 - corections
-        days = months * 28 + parts / PARTS_IN_DAY - 2  # 28 days in month + corections
-        
-        # Time left for round date in corections
-        parts_left_in_week = parts % PARTS_IN_WEEK  # 28 % 7 = 0 so only corections counts
-        parts_left_in_day = parts % PARTS_IN_DAY
-        week_day = parts_left_in_week / PARTS_IN_DAY
-
-        # Special cases of Molad Zaken
-        if ((leap_left < 12 and week_day == 3 and
-             parts_left_in_day >= M(9 + 6, 204)) or
-            (leap_left < 7 and week_day == 2 and
-             parts_left_in_day >= M(15 + 6, 589))):
-            days += 1
-            week_day += 1
-        # ADU
-        if (week_day == 1 or week_day == 4 or week_day == 6):
-            days += 1
-        return days
-
-    def _get_size_of_hebrew_year(self, hebrew_year):
-        """Return: total days in hebrew year"""
-        return self._days_since_3744(hebrew_year + 1) - self._days_since_3744(hebrew_year)
-
-    def _get_year_type(size_of_year, new_year_dw):
-        """Return: type of the year
-Args:
-    size_of_year: Length of year in days
-    new_year_dw First week day of year
-table:
-year type | year length | Tishery 1 day of week
-| 1       | 353         | 2
-| 2       | 353         | 7
-| 3       | 354         | 3
-| 4       | 354         | 5
-| 5       | 355         | 2
-| 6       | 355         | 5
-| 7       | 355         | 7
-| 8       | 383         | 2
-| 9       | 383         | 5
-|10       | 383         | 7
-|11       | 384         | 3
-|12       | 385         | 2
-|13       | 385         | 5
-|14       | 385         | 7
-"""
-        # Only 14 combinations of size and week day are posible
-        year_types = [1, 0, 0, 2, 0, 3, 4, 0, 5, 0, 6, 7, 8, 0, 9, 10, 0, 11, 0, 0, 12, 0, 13, 14]
-        
-        # convert size and first day to 1..24 number
-        # 2,3,5,7 -> 1,2,3,4
-        # 353, 354, 355, 383, 384, 385 -> 0, 1, 2, 3, 4, 5
-        offset = (new_year_dw + 1) / 2
-        offset = offset + 4 * ((size_of_year % 10 - 3) + (size_of_year / 10 - 35))
-
-        return year_types[offset - 1]
-
-    def _gdate_to_jd(self, day, month, year):
-        """Return: The julian day number
-Compute Julian day from Gregorian day, month and year
-Algorithm from the wikipedia's julian_day """
-        a = (14 - month) / 12
-        y = year + 4800 - a
-        m = month + 12 * a - 3
-        jdn = day + (153 * m + 2) / 5 + 365 * y + y / 4 - y / 100 + y / 400 - 32045
-        return jdn
-
-    def _hdate_to_jd(self, day, month, year):
-        """Compute Julian day from Hebrew day, month and year
-Return: julian day number,  1 of tishrey julians,  1 of tishrey julians next year"""
-        if (month == 13):
-            month = 6
-        if (month == 14):
-            month = 6
-            day += 30
-            
-        # Calculate days since 1,1,3744
-        days_from_3744 = self._days_from_3744(year)
-        day = days_from_3744 + (59 * (month - 1) + 1) / 2 + day
-
-        # length of year
-        length_of_year = self._days_from_3744(year + 1) - days_from_3744
-        # Special cases for this year
-        if (length_of_year % 10 > 4 and month > 2):  # long Heshvan
-            day += 1
-        if (length_of_year % 10 < 4 and month > 3):  # short Kislev
-            day -= 1
-        if (length_of_year > 365 and month > 6):  # leap year
-            day += 30
-
-        # adjust to julian
-        jd = day + 1715118
-        jd_tishrey1 = days_from_3744 + 1715119
-        jd_tishrey1_next_year = jd_tishrey1 + length_of_year
-                        
-        return jd, jd_tishrey1, jd_tishrey1_next_year
-
+        self.gdate = date
+        hdate_set_gdate()
     
+    def _set_h_from_jd(self, jd_tishrey1_next_year, jd_tishrey1):
+         = (self._jd + 1) % 7 + 1
+        self._h_size_of_year = jd_tishrey1_next_year - jd_tishrey1
+        self._h_new_year_weekday = (jd_tishrey1 + 1) % 7 + 1
+        self._h_year_type = hj._get_year_type (self._h_size_of_year , self._h_new_year_weekday)
+        self._h_days = self._jd - jd_tishrey1 + 1
+        self._h_weeks = ((self._h_days - 1) + (self._h_new_year_weekday - 1)) / 7 + 1        
+    
+    def hdate_set_gdate(self):
+        self._jd = hj._gdate_to_jd(self.gdate.day, self.gdate.month, self.gdate.year)
+        self._h_day, self._h_month, self._h_year, jd_tishrey1, jd_tishrey1_next_year = hj._jd_to_hdate(self._jd)
+        self._set_h_from_jd(self._jd, jd_tishrey1, jd_tishrey1_next_year)
+        
+    def hdate_set_hdate(self, d, m, y):
+        self._jd, jd_tishrey1, jd_tishrey1_next_year = hj._hdate_to_jd (d, m, y)
+        gd, gm, gy = hj._jd_to_gdate (jd)
+        self.gdate = datetime.date(gy, gm, gd)
+        self._set_h_from_jd(self._jd, jd_tishrey1, jd_tishrey1_next_year)
+        
+    def hdate_set_jd(self, jd):
+        gd, gm, gy = hj._jd_to_gdate(jd)
+        self.gdate = datetime.date(gy, gm, gd)
+        self.hdate_set_gdate()
+    
+    def get_hebrew_date(self):
+        return self._h_day, self._h_month, self._h_year
+        
+    def get_holyday(self, diaspora=False):
+        """return the number of holyday"""
+        holyday = 0
+        # holydays table
+        holydays_table = [
+            [	# Tishrey
+                1, 2, 3, 3, 0, 0, 0, 0, 37, 4,
+                0, 0, 0, 0, 5, 31, 6, 6, 6, 6,
+                7, 27, 8, 0, 0, 0, 0, 0, 0, 0],
+            [	# Heshvan
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 35,
+                35, 35, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [	# Kislev
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 9, 9, 9, 9, 9, 9],
+            [	# Tevet
+                9, 9, 9, 0, 0, 0, 0, 0, 0, 10,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [	# Shvat
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 11, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 33],
+            [	# Adar
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                12, 0, 12, 13, 14, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [	# Nisan
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 15, 32, 16, 16, 16, 16,
+                28, 29, 0, 0, 0, 24, 24, 24, 0, 0],
+            [	# Iyar
+                0, 17, 17, 17, 17, 17, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 18, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 26, 0, 0],
+            [	# Sivan
+                0, 0, 0, 0, 19, 20, 30, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [	# Tamuz
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 21, 21, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 36, 36],
+            [	# Av
+                0, 0, 0, 0, 0, 0, 0, 0, 22, 22,
+                0, 0, 0, 0, 23, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [	# Elul
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [	# Adar 1
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [	# Adar 2
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                12, 0, 12, 13, 14, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            ]
+    
+        # sanity check
+        if (self._h_month < 1 || self._h_month > 14 || self._h_day < 1 || self._h_day > 30):
+            return 0
+        
+        holyday = holydays_table[self._h_month - 1][self._h_day - 1]
+        
+        # if tzom on sat delay one day
+        # tzom gdalyaho on sat
+        if ((holyday == 3) and (self._weekday == 7 || (self._h_day == 4 and self._weekday !=1))):
+            holyday = 0
+        # 17 of Tamuz on sat
+        if ((holyday == 21) and ((self._weekday == 7) || (self._h_day == 18 and self._weekday != 1))):
+            holyday = 0
+        # 9 of Av on sat
+        if ((holyday == 22) and ((self._weekday == 7) || (self._h_day == 10 and self._weekday != 1))):
+            holyday = 0
+        
+        # Hanukah in a long year
+        if ((holyday == 9) and (self._h_size_of_year % 10 != 3) and (self._h_day == 3)):
+            holyday = 0
+        
+        # if tanit ester on sat mov to Thu
+        if ((holyday == 12) and ((self._weekday == 7) || (self._h_day == 11 and self._weekday != 5))):
+            holyday = 0
+        
+        # yom yerushalym after 68
+        if (holyday == 26) and (self.gdate.year < 1968):
+            holyday = 0 
+        
+        # yom ha azmaot and yom ha zicaron
+        if (holyday == 17):
+            if (self.gdate.year < 1948):
+                holyday = 0
+            elif (self.gdate.year < 2004):
+                if ((self._h_day == 3) and (self._weekday == 5)):
+                    holyday = 17
+                elif ((self._h_day == 4) and (self._weekday == 5)):
+                    holyday = 17
+                elif ((self._h_day == 5) and (self._weekday != 6 and self._weekday != 7)):
+                    holyday = 17
+                elif ((self._h_day == 2) and (self._weekday == 4)):
+                    holyday = 25
+                elif ((self._h_day == 3) and (self._weekday == 4)):
+                    holyday = 25
+                elif ((self._h_day == 4) and (self._weekday != 5 and self._weekday != 6)):
+                    holyday = 25
+                else:
+                    holyday = 0
+            else:
+                if ((self._h_day == 3) and (self._weekday == 5)):
+                    holyday = 17
+                elif ((self._h_day == 4) and (self._weekday == 5)):
+                    holyday = 17
+                elif ((self._h_day == 6) and (self._weekday == 3)):
+                    holyday = 17
+                elif ((self._h_day == 5) and (self._weekday != 6 and self._weekday != 7 and self._weekday != 2)):
+                    holyday = 17
+                elif ((self._h_day == 2) and (self._weekday == 4)):
+                    holyday = 25
+                elif ((self._h_day == 3) and (self._weekday == 4)):
+                    holyday = 25
+                elif ((self._h_day == 5) and (self._weekday == 2)):
+                    holyday = 25
+                elif ((self._h_day == 4) and (self._weekday != 5 and self._weekday != 6 and self._weekday != 1)):
+                    holyday = 25
+                else:
+                    holyday = 0
+        
+        # yom ha shoaa, on years after 1958
+        if (holyday == 24):
+            if (self.gdate.year < 1958):
+                holyday = 0
+            else:
+                if ((self._h_day == 26) and (self._weekday != 5)):
+                    holyday = 0
+                if ((self._h_day == 28) and (self._weekday != 2)):
+                    holyday = 0
+                if ((self._h_day == 27) and (self._weekday == 6 || self._weekday == 1)):
+                    holyday = 0
+        
+        # Rabin day, on years after 1997
+        if (holyday == 35):
+            if (self.gdate.year < 1997):
+                holyday = 0
+            else:
+                if ((self._h_day == 10 || self._h_day == 11) and (self._weekday != 5)):
+                    holyday = 0
+                if ((self._h_day == 12) and (self._weekday == 6 || self._weekday == 7)):
+                    holyday = 0
+        
+        # Zhabotinsky day, on years after 2005
+        if (holyday == 36):
+            if (self.gdate.year < 2005):
+                holyday = 0
+            else:
+                if ((self._h_day == 30) and (self._weekday != 1)):
+                    holyday = 0
+                if ((self._h_day == 29) and (self._weekday == 7)):
+                    holyday = 0
+        
+        # diaspora holidays
+        
+        # simchat tora only in diaspora in israel just one day shmini+simchat tora
+        if (holyday == 8 and !diaspora):
+            holyday = 0
+        
+        # sukkot II holiday only in diaspora
+        if (holyday == 31 and !diaspora):
+            holyday = 6
+
+        # pesach II holiday only in diaspora
+        if (holyday == 32 and !diaspora):
+            holyday = 16
+        
+        # shavot II holiday only in diaspora
+        if (holyday == 30 and !diaspora):
+            holyday = 0
+        
+        # pesach VIII holiday only in diaspora
+        if (holyday == 29 and !diaspora):
+            holyday = 0
+        
+        return holyday
+
+    def get_omer_day(self):
+        """return the day of the omer"""
+        sixteen_nissan = HDate()
+        sixteen_nissan.hdate_set_hdate(16, 7, self._h_year)
+        omer_day = self._jd - sixteen_nissan._jd + 1
+        if ((omer_day > 49) or (omer_day < 0)) :
+            omer_day = 0
+
+        return omer_day
