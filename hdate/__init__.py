df8c7a398686796ddaaa86bf0d0dedfbe6f07f61
Tsvi Mostovicz <ttmost@gmail.com>
2017-06-11 16:28:10 +0300

    Fix typo

--- hdate/__init__.py
+++ hdate/__init__.py
@@ -1,5 +1,5 @@
 """
-Jewish calendrical date and times for a giver location.
+Jewish calendrical date and times for a given location.
 
 HDate calculates and generates a represantation either in English or Hebrew
 of the Jewish calendrical date and times for a given location
