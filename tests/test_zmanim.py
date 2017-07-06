cd4ce76609e89e68ddafb6ec7560ae9bcf3ddead
Tsvi Mostovicz <ttmost@gmail.com>
2017-07-06 16:50:15 +0300

    Fix in test: edge case this_date is 29.02 of leap year

--- tests/test_zmanim.py
+++ tests/test_zmanim.py
@@ -21,7 +21,13 @@ class TestZmanim(object):
             else:
                 shift_day = datetime.timedelta(days=1)
 
-        other_date = this_date.replace(year=other_year) + shift_day
+        if (isleap(this_date.year) and not isleap(other_year) and
+                this_date.month == 2 and this_date.day == 29):
+            # Special case we can't simply replace the year as there's
+            # no leap day in the other year
+            other_date = datetime.date(other_year, 3, 1)
+        else:
+            other_date = this_date.replace(year=other_year) + shift_day
 
         assert (hdate.Zmanim(this_date).get_utc_sun_time_full() ==
                 hdate.Zmanim(other_date).get_utc_sun_time_full())
