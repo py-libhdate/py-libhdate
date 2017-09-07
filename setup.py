4f0f27cd347529e0e549896a9058c891bfc4bbe9
Tsvi Mostovicz <ttmost@gmail.com>
2017-09-07 16:15:41 +0300

    Prepare for 0.3 release

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from setuptools import setup
 
 setup(name='hdate',
-      version='0.1',
+      version='0.3',
       description='Hebrew date and Zmanim',
       url='https://github.com/royi1000/py-libhdate',
+      classifiers=[
+           'Development Status :: 5 - Production/Stable',
+           'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+           'Programming Language :: Python :: 2.7'
+      ],
       author='Royi Reshef',
       author_email='roy.myapp@gmail.com',
-      license='MIT',
+      maintainer='Tsvi Mostovicz',
+      maintainer_email='ttmost@gmail.com',
+      license='GPLv3+',
       packages=['hdate'],
       install_requires=[
         'python-dateutil',
