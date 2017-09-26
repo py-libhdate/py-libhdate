2b143be69cd3ca929ceb0eb5fe9b2aa2372b7870
Tsvi Mostovicz <ttmost@gmail.com>
2017-09-26 22:39:21 +0300

    Remove more pylint warnings as well as code unused due to refactoring of get_reading()

--- src/hdate/hdate_julian.py
+++ src/hdate/hdate_julian.py
@@ -68,46 +68,6 @@ def get_size_of_hebrew_year(hebrew_year):
     return _days_from_3744(hebrew_year + 1) - _days_from_3744(hebrew_year)
 
 
-def get_year_type(size_of_year, new_year_dw):
-    """
-    Return: type of the year.
-
-    Args:
-    size_of_year: Length of year in days
-    new_year_dw First week day of year
-    table:
-    year type | year length | Tishery 1 day of week
-    | 1       | 353         | 2
-    | 2       | 353         | 7
-    | 3       | 354         | 3
-    | 4       | 354         | 5
-    | 5       | 355         | 2
-    | 6       | 355         | 5
-    | 7       | 355         | 7
-    | 8       | 383         | 2
-    | 9       | 383         | 5
-    |10       | 383         | 7
-    |11       | 384         | 3
-    |12       | 385         | 2
-    |13       | 385         | 5
-    |14       | 385         | 7
-    """
-    # Only 14 combinations of size and week day are posible
-    year_types = [1, 0, 0, 2, 0, 3, 4,
-                  0, 5, 0, 6, 7, 8, 0,
-                  9, 10, 0, 11, 0, 0, 12,
-                  0, 13, 14]
-
-    # convert size and first day to 1..24 number
-    # 2,3,5,7 -> 1,2,3,4
-    # 353, 354, 355, 383, 384, 385 -> 0, 1, 2, 3, 4, 5
-    offset = (new_year_dw + 1) // 2
-    offset = offset + 4 * ((size_of_year % 10 - 3) +
-                           (size_of_year // 10) - 35)
-
-    return year_types[offset - 1]
-
-
 def gdate_to_jdn(day, month, year):
     """
     Compute Julian day from Gregorian day, month and year.
