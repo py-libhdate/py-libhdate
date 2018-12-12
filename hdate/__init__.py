5a2c823893e22f86d1b17667155633b03dd1835e
Ari Gilder <agilder@gmail.com>
2018-12-11 22:18:08 -0500

    Fix some reading bugs (+cleanup), propagate diaspora/hebrew to sub-HDates

--- hdate/__init__.py
+++ hdate/__init__.py
@@ -8,4 +8,4 @@ from hdate.common import HebrewDate, Location
 from hdate.date import HDate
 from hdate.zmanim import Zmanim
 
-__all__ = ['HDate', 'Zmanim', 'HebrewDate', 'Location']
+__all__ = ['HDate', 'Zmanim', 'HebrewDate', 'Location', 'HolidayTypes']
