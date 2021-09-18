a5c7062cc97ee4858b04e792930e4050b8a2142a
Yuval Aboulafia <yuval.abou@gmail.com>
2021-09-19 00:02:32 +0300

    misses

--- tests/test_hdate.py
+++ tests/test_hdate.py
@@ -739,9 +739,9 @@ class TestHDateReading(object):
         mydate.gdate += tdelta
 
         shabatot = [item for subl in parshiyot for item in subl]
-        for shabat in shabatot:
+        for shabbat in shabatot:
             print("Testing: ", mydate)
-            assert mydate.get_reading() == shabat
+            assert mydate.get_reading() == shabbat
             mydate.gdate += datetime.timedelta(days=7)
         mydate.hdate = HebrewDate(year, 1, 22)
         # VeZot Habracha in Israel always falls on 22 of Tishri
@@ -757,9 +757,9 @@ class TestHDateReading(object):
         mydate.gdate += tdelta
 
         shabatot = [item for subl in parshiyot for item in subl]
-        for shabat in shabatot:
+        for shabbat in shabatot:
             print("Testing: ", mydate)
-            assert mydate.get_reading() == shabat
+            assert mydate.get_reading() == shabbat
             mydate.gdate += datetime.timedelta(days=7)
         mydate.hdate = HebrewDate(year, 1, 23)
         # VeZot Habracha in Israel always falls on 22 of Tishri
