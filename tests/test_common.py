63018d336d07c07be84502c8b9614a1bcbfc2171
Tsvi Mostovicz <ttmost@gmail.com>
2022-10-25 18:41:57 +0300

    Some more python 2 deprecation

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,17 +1,21 @@
-"""Test classes."""
-from __future__ import print_function
+"""Tests for common classes."""
 
+# Imports are needed for evaluating the class.
+# pylint: disable=unused-import
+# It is OK for these test cases to use eval.
+# pylint: disable=eval-used
+import copy
 import datetime  # noqa: F401
 
 import pytest
 
 from hdate import HDate, Location, Zmanim  # noqa: F401
 
-# pylint: disable=no-self-use
 # pylint-comment: In tests, classes are just a grouping semantic
 
 
 class TestClasses:
+    """Wrapper class for tests."""
 
     CLASSES = ["HDate()", "Zmanim()", "Location()"]
 
@@ -22,23 +26,24 @@ class TestClasses:
     @pytest.fixture
     def _copy(self):
         def class_copy(original):
-            import copy
-
             return copy.deepcopy(original)
 
         return class_copy
 
     def test_repr(self, _class):
+        """Test that eval of repr returns a class equal to the class itself."""
         assert _class == eval(repr(_class))
         assert _class is not eval(repr(_class))
 
     def test_equality(self, _class, _copy):
+        """Test classes to be equal."""
         copy_ = _copy(_class)
         copy_.foo = "bar"
         assert not _class == copy_
         assert not _class == "not a class instance"
 
     def test_inequality(self, _class, _copy):
+        """Test inequality."""
         copy_ = _copy(_class)
         copy_.foo = "bar"
         assert _class != copy_
