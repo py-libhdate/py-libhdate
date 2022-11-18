d8bb978c29f780a6149bdc0147df90f239b893c6
Stephen Littman <Stephen.littman7912@gmail.com>
2022-11-18 03:51:30 -0500

    Fix spelling (#108)
    Spelling fix

--- hdate/__init__.py
+++ hdate/__init__.py
@@ -1,7 +1,7 @@
 """
 Jewish calendrical date and times for a given location.
 
-HDate calculates and generates a represantation either in English or Hebrew
+HDate calculates and generates a representation either in English or Hebrew
 of the Jewish calendrical date and times for a given location
 """
 from hdate.common import HebrewDate, Location
