588d4ef809836892b1b69a1114e63cd7524091c5
SLaks <Schabse@GMail.com>
2024-03-09 14:42:56 -0500

    Fix issues with get_holidays_for_year() (#117)
    * Fix get_holidays_for_year() to return conditional holidays
    
    It had applied the filter callbacks on the date being called on, so it would only return filtered dates when called on that date.
    
    For example, .get_holidays_for_year(FAST_DAY) would only return 10 Tevet (and the date it was called on, if called with a fast day)
    
    * Don't return two copies of Purim for every year
    
    * Move adar calculation into lambda function in htables
    
    ---------
    
    Co-authored-by: Tsvi Mostovicz <ttmost@gmail.com>

--- tests/test_hdate.py
+++ tests/test_hdate.py
@@ -146,6 +146,32 @@ class TestHDate:
 class TestSpecialDays:
     """Test HDate in terms of special days."""
 
+    # Test against both a leap year and non-leap year
+    @pytest.mark.parametrize(("year"), ((5783, 5784)))
+    def test_get_holidays_for_year(self, year):
+        """Test that get_holidays_for_year() returns every holiday."""
+        cur_date = HDate(heb_date=HebrewDate(year, 1, 1))
+        expected_holiday_map = {
+            date.gdate: entry for (entry, date) in cur_date.get_holidays_for_year()
+        }
+        while cur_date.hdate.year == year:
+            actual_holiday = cur_date.holiday_name
+            if actual_holiday:
+                assert actual_holiday == expected_holiday_map[cur_date.gdate].name
+            cur_date = cur_date.next_day
+
+    def test_get_holidays_for_year_non_leap_year(self):
+        """Test that get_holidays_for_year() returns consistent months."""
+        base_date = HDate(heb_date=HebrewDate(5783, 1, 1))
+        for entry, date in base_date.get_holidays_for_year():
+            assert date.hdate.month not in [Months.ADAR_I, Months.ADAR_II]
+
+    def test_get_holidays_for_year_leap_year(self):
+        """Test that get_holidays_for_year() returns consistent months."""
+        base_date = HDate(heb_date=HebrewDate(5784, 1, 1))
+        for entry, date in base_date.get_holidays_for_year():
+            assert date.hdate.month != Months.ADAR
+
     NON_MOVING_HOLIDAYS = [
         ((1, 1), "rosh_hashana_i"),
         ((2, 1), "rosh_hashana_ii"),
