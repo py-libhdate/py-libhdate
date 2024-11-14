ff488914d5b0d1acd0121b49a7bfaa08437bcd76
Dan BENDAVID <38190460+DanBendavid@users.noreply.github.com>
2024-11-14 22:41:46 +0100

    modified:   hdate/htables.py 	modified:   hdate/zmanim.py 	modified:   tests/test_hdate_api.py

--- hdate/htables.py
+++ hdate/htables.py
@@ -900,6 +900,7 @@ ZMANIM = (
     ZMAN("gra_end_tfila", LANG('Tefila GR"A', 'Tefila EOT GR"A', 'סוף זמן תפילה גר"א')),
     ZMAN("midday", LANG("Hatsot", "Midday", "חצות היום")),
     ZMAN("big_mincha", LANG("Minha Guedola", "Big Mincha", "מנחה גדולה")),
+    ZMAN("big_mincha_30", LANG("Minha Guedola 30", "Big Mincha 30", "מנחה גדולה 30")),
     ZMAN("small_mincha", LANG("Minha Qetana", "Small Mincha", "מנחה קטנה")),
     ZMAN("plag_mincha", LANG("Plag haMinha", "Plag Mincha", "פלג המנחה")),
     ZMAN("sunset", LANG("Shqiat", "Sunset", "שקיעה")),
