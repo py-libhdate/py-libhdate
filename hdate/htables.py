6fb55c96105b619443893035fa8971d2e8c70191
Tsvi Mostovicz <ttmost@gmail.com>
2024-12-11 17:28:53 +0200

    Change implementation

--- hdate/htables.py
+++ hdate/htables.py
@@ -2,6 +2,7 @@
 
 import datetime
 from collections import namedtuple
+from dataclasses import dataclass
 from enum import Enum, IntEnum
 from typing import Callable, TypeVar
 
@@ -811,45 +812,57 @@ ZMANIM = (
 # that, the math doesn't play nicely with the dates before the 11th cycle :(
 # From cycle 11 onwards, it was simple and sequential
 DAF_YOMI_CYCLE_11_START = datetime.date(1997, 9, 29)
-MESECHTA = namedtuple("MESECHTA", ["name", "pages"])
+
+
+@dataclass
+class Masechta(TranslatorMixin):
+    """Masechta object."""
+
+    name: str
+    pages: int
+
+    def __str__(self) -> str:
+        return self.get_translation(self.name)
+
+
 DAF_YOMI_MESECHTOS = (
-    MESECHTA(LANG("Berakhot", "Berachos", "ברכות"), 63),
-    MESECHTA(LANG("Shabbat", "Shabbos", "שבת"), 156),
-    MESECHTA(LANG("Erouvin", "Eruvin", "עירובין"), 104),
-    MESECHTA(LANG("Pessa'him", "Pesachim", "פסחים"), 120),
-    MESECHTA(LANG("Chekalim", "Shekalim", "שקלים"), 21),
-    MESECHTA(LANG("Yoma", "Yoma", "יומא"), 87),
-    MESECHTA(LANG("Soucca", "Succah", "סוכה"), 55),
-    MESECHTA(LANG("Beitsa", "Beitzah", "ביצה"), 39),
-    MESECHTA(LANG("Roch Hachana", "Rosh Hashanah", "ראש השנה"), 34),
-    MESECHTA(LANG("Ta'anit", "Taanis", "תענית"), 30),
-    MESECHTA(LANG("Meguila", "Megillah", "מגילה"), 31),
-    MESECHTA(LANG("Moëd Katan", "Moed Katan", "מועד קטן"), 28),
-    MESECHTA(LANG("Haguiga", "Chagigah", "חגיגה"), 26),
-    MESECHTA(LANG("Yevamot", "Yevamos", "יבמות"), 121),
-    MESECHTA(LANG("Ketouvot", "Kesubos", "כתובות"), 111),
-    MESECHTA(LANG("Nédarim", "Nedarim", "נדרים"), 90),
-    MESECHTA(LANG("Nazir", "Nazir", "נזיר"), 65),
-    MESECHTA(LANG("Sota", "Sotah", "סוטה"), 48),
-    MESECHTA(LANG("Guittin", "Gittin", "גיטין"), 89),
-    MESECHTA(LANG("Kidouchin", "Kiddushin", "קידושין"), 81),
-    MESECHTA(LANG("Baba Kama", "Bava Kamma", "בבא קמא"), 118),
-    MESECHTA(LANG("Baba Metsia", "Bava Metzia", "בבא מציעא"), 118),
-    MESECHTA(LANG("Baba Batra", "Bava Basra", "בבא בתרא"), 175),
-    MESECHTA(LANG("Sanhedrin", "Sanhedrin", "סנהדרין"), 112),
-    MESECHTA(LANG("Makot", "Makkos", "מכות"), 23),
-    MESECHTA(LANG("Chevouot", "Shevuos", "שבועות"), 48),
-    MESECHTA(LANG("Avoda Zara", "Avodah Zarah", "עבודה זרה"), 75),
-    MESECHTA(LANG("Horayot", "Horayos", "הוריות"), 13),
-    MESECHTA(LANG("Zevahim", "Zevachim", "זבחים"), 119),
-    MESECHTA(LANG("Menahot", "Menachos", "מנחות"), 109),
-    MESECHTA(LANG("Houlin", "Chullin", "חולין"), 141),
-    MESECHTA(LANG("Bekhorot", "Bechoros", "בכורות"), 60),
-    MESECHTA(LANG("Arakhin", "Arachin", "ערכין"), 33),
-    MESECHTA(LANG("Temoura", "Temurah", "תמורה"), 33),
-    MESECHTA(LANG("Keritot", "Kereisos", "כריתות"), 27),
-    MESECHTA(LANG("Me'ila", "Meilah", "מעילה"), 36),
-    MESECHTA(LANG("Nida", "Niddah", "נדה"), 72),
+    Masechta("berachos", 63),
+    Masechta("shabbos", 156),
+    Masechta("eruvin", 104),
+    Masechta("pesachim", 120),
+    Masechta("shekalim", 21),
+    Masechta("yoma", 87),
+    Masechta("succah", 55),
+    Masechta("beitzah", 39),
+    Masechta("rosh_hashanah", 34),
+    Masechta("taanis", 30),
+    Masechta("megillah", 31),
+    Masechta("moed_katan", 28),
+    Masechta("chagigah", 26),
+    Masechta("yevamos", 121),
+    Masechta("kesubos", 111),
+    Masechta("nedarim", 90),
+    Masechta("nazir", 65),
+    Masechta("sotah", 48),
+    Masechta("gittin", 89),
+    Masechta("kiddushin", 81),
+    Masechta("bava_kamma", 118),
+    Masechta("bava_metzia", 118),
+    Masechta("bava_basra", 175),
+    Masechta("sanhedrin", 112),
+    Masechta("makkos", 23),
+    Masechta("shevuos", 48),
+    Masechta("avodah_zarah", 75),
+    Masechta("horayos", 13),
+    Masechta("zevachim", 119),
+    Masechta("menachos", 109),
+    Masechta("chullin", 141),
+    Masechta("bechoros", 60),
+    Masechta("arachin", 33),
+    Masechta("temurah", 33),
+    Masechta("kereisos", 27),
+    Masechta("meilah", 36),
+    Masechta("niddah", 72),
 )
 
 DAF_YOMI_TOTAL_PAGES = sum(mesechta.pages for mesechta in DAF_YOMI_MESECHTOS)
