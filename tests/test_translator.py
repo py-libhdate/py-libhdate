e94b5865092648a685e76cd6d17594b858d5b716
Tsvi Mostovicz <ttmost@gmail.com>
2024-12-11 11:47:17 +0200

    Simplify tests

--- tests/test_translator.py
+++ tests/test_translator.py
@@ -1,41 +1,17 @@
 """Tests for the TranslatorMixin class."""
 
-import pytest
-
-from hdate.translator import TranslatorMixin
-
-# pylint: disable=redefined-outer-name
+from hdate.htables import Months
 
 
-@pytest.fixture
-def translator() -> type[TranslatorMixin]:
-    """Return the TranslatorMixin class."""
-
-    class Translator(TranslatorMixin):
-        """Dummy Translator class."""
-
-        TISHREI = 1
-
-    return Translator
-
-
-@pytest.fixture
-def translator_instance(translator: type[TranslatorMixin]) -> TranslatorMixin:
-    """Return an instance of the TranslatorMixin class."""
-    return translator()
-
-
-def test_available_languages(translator_instance: TranslatorMixin) -> None:
+def test_available_languages() -> None:
     """Test the available_languages method."""
     known_languages = ["english", "french", "hebrew"]
+    month = Months.TISHREI
     for language in known_languages:
-        assert language[:2] in translator_instance.available_languages()
+        assert language[:2] in month.available_languages()
 
 
-def test_load_language(translator: type[TranslatorMixin]) -> None:
+def test_load_language() -> None:
     """Test the load_language method."""
-    translator.__name__ = "months"
-
-    inst = translator()
-    inst.load_translations()
-    assert inst.get_translation("TISHREI") == "Tishrei"
+    month = Months.TISHREI
+    assert str(month) == "Tishrei"
