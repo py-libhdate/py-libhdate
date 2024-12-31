76ef6f52968c66de5b128d7aa9e6fe8534a7fc30
Dan BENDAVID <38190460+DanBendavid@users.noreply.github.com>
2024-12-31 18:36:52 +0100

    modified:   hdate/tekufot.py

--- hdate/tekufot.py
+++ hdate/tekufot.py
@@ -40,7 +40,7 @@ class Tekufot(TranslatorMixin):  # pylint: disable=too-many-instance-attributes
     periods for prayer insertions, and associated halachic dates such as
     the start of Cheilat Geshamim (requesting rain)."""
 
-    # pylint: disable=too-many-positional-arguments
+    # pylint: disable=too-many-arguments, R0917
     def __init__(
         self,
         date: dt.date = dt.datetime.now(),
