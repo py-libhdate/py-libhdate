3ca7b960b73b97770cc82c8a52e5649c1953c9a2
Tsvi Mostovicz <ttmost@gmail.com>
2025-01-14 14:18:24 +0200

    Fix lowering of coverage

--- tests/test_translator.py
+++ tests/test_translator.py
@@ -47,3 +47,14 @@ def test_str_without_name() -> None:
     foo_class = Foo()
     with pytest.raises(NameError):
         str(foo_class)
+
+
+def test_translation_not_found(caplog: pytest.LogCaptureFixture) -> None:
+    """Test the get_translation method when no translation is available."""
+
+    class Foo(TranslatorMixin):
+        """Test class."""
+
+    foo_class = Foo()
+    assert foo_class.get_translation("non-existing-key") == "non-existing-key"
+    assert "Translation for non-existing-key not found" in caplog.text
